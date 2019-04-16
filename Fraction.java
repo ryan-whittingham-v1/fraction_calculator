d184f2f16de5f71d487cf61d40ea9b2260fd3841
Ryan Whittingham <whittingham777@gmail.com>
2019-04-15 17:42:22 -0700

    Fraction class divide, add, neg handling, divide by zero

--- Fraction.java
+++ Fraction.java
@@ -2,14 +2,21 @@
 Java class that handles fraction numbers
 */
 
-public class Fraction {
+class Fraction {
 
 	private int num, denom; //numerator and denominator
 	
 	//Contstructor that handles two integer inputs 
 	public Fraction(int top, int bottom){
+		
 		num = top;
 		denom = bottom;
+	
+		if(denom < 0){//Negative denominator fix
+			num = (num * -1);
+			denom = (denom * -1);
+		}
+
 	}
 
 	//Contstructor that handles one integer input
@@ -25,15 +32,65 @@ public class Fraction {
 	
 	//Method that returns the denominator of the fraction
 	public int getDenom(){
-		return num;
+		return denom;
 	}
 
 	//Method that overides toString to return fraction as a string
 	public String toString(){
+		
+		if(denom == 0){ //If fraction is undefined
+			return "NaN";
+		}
+
 		return (num + "/" + denom);
 	}
 
+	//Method that multiplies two fractions
+	public Fraction mul(Fraction n){
 
+		Fraction result = new Fraction(0, 0); //Create new fraction
+		
+		if(denom == 0 || n.getDenom() == 0){ //If dealing with undefined numbers 
+			return result;			
+		}
 
+		int topResult = (num * n.getNum());
+		int botResult = (denom * n.getDenom());	
+		result = new Fraction(topResult, botResult); //Create new fraction
+		return result;
+		
+	}
+	
+	//Method that divides two fractions
+	public Fraction div(Fraction n){
 
+		Fraction result = new Fraction(0, 0); //Create new fraction
+		
+		if(denom == 0 || n.getDenom() == 0){ //If dealing with undefined numbers 
+			return result;			
+		}
+
+		int topResult = (num * n.getDenom());
+		int botResult = (denom * n.getNum());	
+		result = new Fraction(topResult, botResult); //Create new fraction
+		return result;
+		
+	}
+	
+	//Method that adds two fractions
+	public Fraction add(Fraction n){
+
+		Fraction result = new Fraction(0, 0); //Create new fraction
+		
+		if(denom == 0 || n.getDenom() == 0){ //If dealing with undefined numbers 
+			return result;			
+		}
+		
+		int topResult = ((num * n.getDenom()) + (denom * n.getNum()));
+		int botResult = (denom * n.getDenom());	
+		result = new Fraction(topResult, botResult); //Create new fraction
+		return result;
+		
+	}
 }
+	
--- Main.java
+++ Main.java
@@ -1,13 +1,40 @@
 public class Main {
 	
 	public static void main(String[] args) {
+		System.out.println("Multipy positive");	
+		Fraction fracA = new Fraction(1,3);
+		Fraction fracB = new Fraction(2,5);
+		System.out.println("Fraction A 1/3: " +  fracA);
+		System.out.println("Fraction B 2/5: " + fracB);
+		System.out.println("Fraction A * Fraction B 2/15: " + fracA.mul(fracB));
 		
-		Fraction frac1 = new Fraction(1,2);// 1/2
-		Fraction frac2 = new Fraction(4);// 4/1
-
-		System.out.println("Fraction 1: " +  frac1);
-		System.out.println("Fraction 2: " + frac2);
+		System.out.println("Multipy negative");	
+		fracA = new Fraction(1,-3);
+		fracB = new Fraction(2,5);
+		System.out.println("Fraction A -1/3: " +  fracA);
+		System.out.println("Fraction B 2/5: " + fracB);
+		System.out.println("Fraction A * Fraction B -2/15: " + fracA.mul(fracB));
 		
+		System.out.println("Multipy 0");	
+		fracA = new Fraction(1,0);
+		fracB = new Fraction(2,5);
+		System.out.println("Fraction A 1/0: " +  fracA);
+		System.out.println("Fraction B 2/5: " + fracB);
+		System.out.println("Fraction A * Fraction B NaN: " + fracA.mul(fracB));
+		
+		System.out.println("Divide");	
+		fracA = new Fraction(4,5);
+		fracB = new Fraction(3,20);
+		System.out.println("Fraction A 4/5: " +  fracA);
+		System.out.println("Fraction B 3/20: " + fracB);
+		System.out.println("Fraction A * Fraction B 80/15: " + fracA.div(fracB));
+		
+		System.out.println("Add");	
+		fracA = new Fraction(1,4);
+		fracB = new Fraction(2,5);
+		System.out.println("Fraction A 1/4: " +  fracA);
+		System.out.println("Fraction B 2/5: " + fracB);
+		System.out.println("Fraction A * Fraction B 13/20: " + fracA.add(fracB));
 	}
 
 }
